88ee8d273cc62724e173de67623465d8d2744671
dlccyes <derricken968@gmail.com>
2022-06-06 01:16:16 +0800

    vue: add theme switcher

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,15 +1,59 @@
+<script setup>
+import { globeStore } from '@/stores/globe'
+</script>
 <script>
 export default {
+  data() {
+    return {
+      globe:  globeStore(),
+    }
+  },
   computed: {
     isHome(){
       return this.$route.name == 'home';
+    },
+  },
+  methods: {
+    meditate(){
+      if(this.globe.night == 1){
+        document.getElementById("night-btn").innerHTML = "🌞";
+        document.documentElement.dataset.theme = "day";
+        this.globe.night = 0;
+      }else{
+        document.getElementById("night-btn").innerHTML = "🌝";
+        document.documentElement.dataset.theme = "night";
+        this.globe.night = 1;
+      }
+      this.globe.update++;
+    },
+    nightEnter(){
+      if(this.globe.night == 1){
+        document.getElementById("night-btn").innerHTML = "🌝";
+      }else{
+        document.getElementById("night-btn").innerHTML = "🌞";
+      }
+    },
+    nightLeave(){
+      if(this.globe.night == 1){
+        document.getElementById("night-btn").innerHTML = "🌙";
+      }else{
+        document.getElementById("night-btn").innerHTML = "⛅";
+      }
     }
-  }
+  },
+  mounted() {
+    document.getElementById("night-btn").addEventListener("mouseenter", this.nightEnter);
+    document.getElementById("night-btn").addEventListener("mouseleave", this.nightLeave);
+  },
 }
 </script>
+
 <template>
-  <div id="nav">
-    <router-link v-if="!isHome" to="/">Dashboard</router-link>
-  </div>
-  <router-view/>
+
+<span id="night-btn" @click="meditate()">🌙</span>
+<div id="nav">
+  <router-link v-if="!isHome" to="/">Dashboard</router-link>
+</div>
+<router-view/>
+
 </template>
